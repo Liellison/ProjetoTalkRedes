2f899b86bd6ac43bad7169bdff4f9d63cd72de21
liellison <liellisonjoseph@gmail.com>
2016-09-21 19:00:11 -0300

    Versao 0.0.2.1 Criacao do servidor TCP

--- servidor/tcp/servtcp.java
+++ servidor/tcp/servtcp.java
@@ -1,1 +1,23 @@
-/* TODO  criaçao do servidor tcp*/
+/*Este codigo do servidor foi passado pelo professor da disciplina, inicialmente ele faz a letra fica em caixa alta*/
+import java.io.*;
+import java.net.*;
+
+class TCPServer{
+    public static void main(String argv[]) throws Exception{
+        String clienteSentence;
+        String capitalizerSentence;
+
+        ServerSocket welcomeSocket = new ServerSocket(6798);
+        while (true){
+            Socket connectionSocket = welcomeSocket.accept();
+
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+
+            clienteSentence = inFromClient.readLine();
+            capitalizerSentence = clienteSentence.toUpperCase()+ "\n";
+
+            outToClient.writeBytes(capitalizerSentence);
+        }
+    }
+}
