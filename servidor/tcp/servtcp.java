e3ed33c10769b180191fb11a5922db0419f5b455
liellison <liellisonjoseph@gmail.com>
2016-09-21 19:08:25 -0300

    Versao 0.0.2.1.1 Criacao dos comentarios no  servidor TCP

--- servidor/tcp/servtcp.java
+++ servidor/tcp/servtcp.java
@@ -1,22 +1,22 @@
 /*Este codigo do servidor foi passado pelo professor da disciplina, inicialmente ele faz a letra fica em caixa alta*/
-import java.io.*;
-import java.net.*;
-
 class TCPServer{
     public static void main(String argv[]) throws Exception{
         String clienteSentence;
         String capitalizerSentence;
-
+        /*Cria socket de apresentaçao na porta 6789*/
         ServerSocket welcomeSocket = new ServerSocket(6798);
         while (true){
+        /*Espera no socket de apresentacao pelo contato do cliente*/
             Socket connectionSocket = welcomeSocket.accept();
-
+        /*Cria cadeia de entrada, conectada ao socket*/
             BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+        /*Cria cadeia de saida, conectada ao socket*/
             DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-
+        /*Le linha do socket*/
             clienteSentence = inFromClient.readLine();
+        /*Coloca o que esta na linha do socket em caixa alta*/
             capitalizerSentence = clienteSentence.toUpperCase()+ "\n";
-
+        /*Escreve linha no socket*/
             outToClient.writeBytes(capitalizerSentence);
         }
     }
