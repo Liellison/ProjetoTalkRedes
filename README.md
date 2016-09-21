7da29932fbec9247ab95b0791b3a5a7abb114e17
liellison <liellisonjoseph@gmail.com>
2016-09-21 18:40:13 -0300

    Atualizacao do readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# weevravvevsd
+# Talk Simple
+
+O app tem como objetivo realizar a troca de strings entre dois telefones com o sistema operacional Android.
+
+Inicialmente o projeto saiu de um trabalho na disciplina de Redes de Computadores no semestre 2016.1 na UERN-Natal.
+
+O objetivo do projeto da disciplina era de usar tanto um servidor TCP como um UDP para realizar calculos numericos, onde o usuario iria dar valores e o servidor iria retornar com o calculo. Porem, me senti mais motivado a realizar a troca de strings.
--- app/src/main/java/com/liellison/projetotalkredes/MainActivity.java
+++ app/src/main/java/com/liellison/projetotalkredes/MainActivity.java
@@ -1,13 +1,23 @@
 package com.liellison.projetotalkredes;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.TableLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    private TableLayout tabLayout;
 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout = (TableLayout) findViewById(R.id.tabLayout);
+
     }
+
+
 }
