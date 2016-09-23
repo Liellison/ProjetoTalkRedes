18503553e206d53572305e10c77cf7e88eeca40d
liellison <liellisonjoseph@gmail.com>
2016-09-23 18:59:26 -0300

    Versao 0.0.2.1.2 Alteracao na impressao do servidor

--- servidor/tcp/servtcp.java
+++ servidor/tcp/servtcp.java
@@ -1,4 +1,11 @@
 /*Este codigo do servidor foi passado pelo professor da disciplina, inicialmente ele faz a letra fica em caixa alta*/
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 class TCPServer{
     public static void main(String argv[]) throws Exception{
         String clienteSentence;
@@ -8,11 +15,14 @@ class TCPServer{
         while (true){
         /*Espera no socket de apresentacao pelo contato do cliente*/
             Socket connectionSocket = welcomeSocket.accept();
+            System.out.println("\n IP: " + connectionSocket.getInetAddress() + " - Conectado!\n");
         /*Cria cadeia de entrada, conectada ao socket*/
             BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
         /*Cria cadeia de saida, conectada ao socket*/
             DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-        /*Le linha do socket*/
+            InetAddress IPAddress = connectionSocket.getInetAddress();
+            int port = connectionSocket.getPort();
+            /*Le linha do socket*/
             clienteSentence = inFromClient.readLine();
         /*Coloca o que esta na linha do socket em caixa alta*/
             capitalizerSentence = clienteSentence.toUpperCase()+ "\n";
